62616a9b1903aee6788bf678d8c87a1d1f4d6f2d
jaz <jazgcabriza@gmail.com>
2026-03-16 13:55:41 +0800

    camera boundaries

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -2,9 +2,24 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
+    public Transform target; //the pig character
+    //map boundaries to show on the camera
+    public float minY;
+    public float maxY;
+    public float minX;
+    public float maxX;
+
     private void LateUpdate()
     {
-        transform.position = new Vector3(target.position.x,target.position.y,transform.position.z);
+        //get the target's current position
+        float targetX=target.position.x;
+        float targetY=target.position.y;
+
+        // Clamp the camera position so it doesn't go beyond the specified limits
+        float clampX= Mathf.Clamp(targetX,minX,maxX);
+        float clampY= Mathf.Clamp(targetY,minY,maxY);
+
+        //update the cameras position
+        transform.position = new Vector3( clampX, clampY,transform.position.z);
     }
 }
